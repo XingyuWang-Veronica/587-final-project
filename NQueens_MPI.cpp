bd7e73e3505591f73d8a87191cb8600a75a29aef
Xingyu Wang <xingyuw@br018.pvt.bridges.psc.edu>
2020-01-20 15:41:09 -0500

    Gonna go to school

--- NQueens_MPI.cpp
+++ NQueens_MPI.cpp
@@ -1,5 +1,8 @@
 #include "mpi.h"
+#include "omp.h"
 #include <iostream>
+#include <queue>
+
 
 using namespace std;
 
@@ -8,10 +11,45 @@ int main(int argc, char * argv[]) {
     cerr << "Format: <exe> <N>\n";
     return 1;
   }
-  int p, id;
-  MPI_INIT(&argc, &argv);
+  int p, id, provided;
+  // MPI_INIT(&argc, &argv);
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
-
+  // initialization: tasks by default
+  queue<> local_tasks;
+  // a thread dedicated to listening to:
+  // (1) "STOP" -- have found the solution
+  // (2) "Give me more tasks" -- use mutex
+  // (3) "FIN" -- no solution is found
+  // a thread dedicated to update:
+  // (1) update the local queue -- use mutex
+  // (2) if there are no tasks left, send "Give me more tasks"
+  //     to each of the other processes and wait for responses,
+  //     then go to (1) or (4)
+  // (3) if have found the solution, send "STOP" to all the 
+  //     other processes and print the output
+  // (4) if all the processes respond with no more tasks, return
+  //     and if this is process 0, print "No solution found"
+  omp_set_num_threads(2);
+  int ithread;
+  bool stop = false, fin = false;
+  omp_lock_t mutex;
+  omp_init_lock(&mutex);
+  #pragma omp parallel private(ithread)
+  {
+    ithread = omp_get_thread_num();
+    if (!ithread) {
+      while (!stop && !fin) {
+      
+      }   
+    } else {
+      while (!stop) {
+        omp_set_lock(&mutex);
+        
+        omp_unset_lock(&mutex);
+      }
+    }
+  }
   return 0;
 }
--- psc_bash_script.bat
+++ psc_bash_script.bat
@@ -11,10 +11,10 @@
 #SBATCH -p RM-shared
 
 # Specify the number of cores:
-#SBATCH --ntasks-per-node 1
+#SBATCH --ntasks-per-node 7
 
 # Specify your max walltime:
-#SBATCH -t 0:05:00
+#SBATCH -t 0:10:00
 
 
 # Replace <username> below with your PSC username.
@@ -22,13 +22,13 @@
 # Move to your working directory (in pylon5):
 cd /pylon5/sc31l6p/xingyuw
 # Load your program:
-cp $HOME/587-final-project/NQueens_OpenMP .
+cp $HOME/587-final-project/NQueens_OpenMP_random .
 
 # Run your OpenMP program
 # (">" redirects the print output of your program,
 #  in this case to "NQueens_OpenMP.txt" in pylon5)
 
-export OMP_NUM_THREADS=1
-./NQueens_OpenMP 8 > /pylon5/sc31l6p/xingyuw/NQueens_OpenMP_8.txt
+export OMP_NUM_THREADS=7
+./NQueens_OpenMP_random 8 > /pylon5/sc31l6p/xingyuw/NQueens_OpenMP_8_t7_random.txt
 
 
